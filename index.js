decb6f1e61889ce83ef6ac4de80d7f3a4ba8e4cb
Jesse <jessyvee1@gmail.com>
2023-03-23 22:33:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
 
-const titleCased = () => {
-  return tutorials
-}
+function titlecased(){
+  return tutorials.map(function(title) {
+  let words = title.split(' ');
+  for (let i = 0; i < words.length; i++) {
+  words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+  return words.join(' ') ;
+  });
+  }
+
+  //Example usage:
+  console.log(titlecased());
